040659622839213da8056b5d6721d215813e012e
Ian Young <iyoung@ryerson.ca>
2022-09-21 05:44:16 -0400

    Updated r script files

--- dinesafe_modelling.R
+++ dinesafe_modelling.R
@@ -1,10 +1,9 @@
 
-pacman::p_load(rio,          # File import
-               here,         # File locator
+pacman::p_load(rio,
                brms,
                Matrix,
-               tidyverse,    # data management + ggplot2 graphics
-               tsibble,      # handle time series datasets
+               tidyverse,  
+               tsibble, 
                janitor,
                lubridate,
                tidybayes, 
@@ -14,37 +13,45 @@ pacman::p_load(rio,          # File import
                modelr)
 
 
-# Prep data for analysis - add 0 counts to missing weeks
+# Prep data for analysis 
 
-ts_data <- ts_data %>% fill_gaps(.full = TRUE) %>% 
-  replace(is.na(.), 0)
-
-ts_data <- ts_data %>% 
+ts_data <- ts_data |> 
   mutate(time = row_number(week), 
          pandemic = as.factor(case_when(week >= covid_start ~ "Pandemic Period",
                                         TRUE ~ "Pre-Pandemic")), 
-         time_post = pmax(0, week - covid_start + 1)) %>% 
+         time_post = pmax(0, week - covid_start + 1)) |> 
   mutate(pandemic = fct_relevel(pandemic, "Pre-Pandemic", "Pandemic Period"))
 
-ts_data  %>% tabyl(time_post)
+ts_data  |> tabyl(time_post)
 
 
 # Remove weeks with 0 to only a few inspections in 2020
 
-ts_model <- ts_data %>% filter(inspections > 3)
-ts_model <- ts_model %>% mutate(month = as.factor(month(week)))
+ts_model <- ts_data |> filter(inspections > 3)
+ts_model <- ts_model |> mutate(month = as.factor(month(week)))
 
-ts_model %>% tabyl(pandemic)
+ts_model |> tabyl(pandemic)
 
 
 ### Segmented regression approach using BRMS ###
 
-#### Model pass rate
+#### Model pass rate outcome
 
 get_prior(pass | trials(inspections) ~ 0 + Intercept + time*pandemic + month,
           data = ts_model, family = binomial)
 
 prior <- set_prior("normal(0,1)", class = "b")
+
+m1.1 <- brm(pass | trials(inspections) ~ 0 + Intercept + time*pandemic + month, 
+          data = ts_model, family = binomial, 
+          iter = 4000, chains = 4, cores = 4, warmup = 1000, seed = 9, sample_prior = TRUE, backend = "cmdstanr", 
+          stan_model_args = list(stanc_options = list("O1")))
+
+plot(m1.1)
+pp_check(m1.1, type = "stat", stat = "mean", prefix = "ppd")
+pp_check(m1.1)
+pp_check(m1.1, ndraws=100, prefix = "ppd")
+pp_check(m1.1, type = "loo_pit")
 
 m1 <- brm(pass | trials(inspections) ~ 0 + Intercept + time*pandemic + month, 
           data = ts_model, family = binomial, 
@@ -58,6 +65,8 @@ pp_check(m1, type = "loo_pit")
 mcmc_acf(m1, pars = vars(contains("Pandemic"), contains("time")), lags = 10)
 mcmc_plot(m1)
 conditional_effects(m1)
+
+# Compare to model with auto-regressive term
 
 m2 <- brm(pass | trials(inspections) ~ 0 + Intercept + time*pandemic + month + ar(p = 1), 
           data = ts_model, family = binomial, 
@@ -111,7 +120,7 @@ loo(m3, m4)
 
 
 
-# Summarize model results
+# Summarize best-fitting model results
 
 print(m3, digits = 5)
 
@@ -119,20 +128,20 @@ exp(fixef(m3)[,-2])
 
 get_variables(m3)
 
-m3 %>% gather_draws(r_month[month,]) %>%  mean_qi()
+m3 |> gather_draws(r_month[month,]) |>  mean_qi()
 
 # Plot posterior predictive distributions for expected values/means
 
-# First effect of pandemic
+# First examine effect of pandemic
 
 pred1 <- predictions(m3, type = "response", 
                      newdata = datagrid(model = m3, pandemic = "Pre-Pandemic", 
-                                        time = seq(1, 168, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(1, 168, by = 2), inspections = 1)) |> posteriordraws()
 head(pred1)
 
 pred2 <- predictions(m3, type = "response", 
                      newdata = datagrid(model = m3, pandemic = "Pandemic Period", 
-                                        time = seq(169, 277, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(169, 280, by = 2), inspections = 1)) |> posteriordraws()
 
 pred <- rbind(pred1, pred2)
 remove(pred1, pred2)
@@ -145,7 +154,7 @@ p1 <- ggplot(pred, aes(x = draw, fill = pandemic)) +
   theme(legend.position = "bottom")   
 
 mfx <- marginaleffects(m3, type = "response", variables = "pandemic", 
-                       newdata = datagrid(time = seq(1, 277, by = 2), inspections = 1)) %>% 
+                       newdata = datagrid(time = seq(1, 280, by = 2), inspections = 1)) |> 
   posteriordraws()
 
 p2 <- ggplot(mfx, aes(x = draw)) +
@@ -154,15 +163,15 @@ p2 <- ggplot(mfx, aes(x = draw)) +
        subtitle = "Marginal Effect of Pandemic") +
   theme_minimal() 
 
-cowplot::plot_grid(p1, p2, nrow=1, ncol=2)
+cowplot::plot_grid(p1, p2, labels = c("A", "B"), nrow=1, ncol=2)
 
 remove(p1, p2)
 
 # Alternative plot comparing effect of different time periods
 
 mfx <- marginaleffects(m3, type = "response", variables = "pandemic", 
-                       newdata = datagrid(time = c(mean(169:261), mean(262:277)), 
-                                          inspections = 1)) %>% posteriordraws()
+                       newdata = datagrid(time = c(mean(169:261), mean(262:280)), 
+                                          inspections = 1)) |> posteriordraws()
 
 p2 <- ggplot(mfx, aes(x = draw, fill = factor(time))) +
   stat_halfeye(slab_alpha = .5)  +
@@ -176,17 +185,17 @@ remove(p1, p2)
 # and over 2022 since inspection numbers resumed to 200+ in week 17 (Apr. 25, 2022)
 
 marginaleffects(m3, type = "response", variables = "pandemic", 
-                newdata = datagrid(time = seq(1, 277, by = 2), inspections = 1)) %>% summary()
+                newdata = datagrid(time = seq(1, 280, by = 2), inspections = 1)) |> summary()
 
 marginaleffects(m3, type = "response", variables = "pandemic", 
-                newdata = datagrid(time = seq(1, 261, by = 2), inspections = 1)) %>% summary()
+                newdata = datagrid(time = seq(1, 261, by = 2), inspections = 1)) |> summary()
 
 marginaleffects(m3, type = "response", variables = "pandemic", 
-                newdata = datagrid(time = seq(262, 277, by = 2), inspections = 1)) %>% summary()
+                newdata = datagrid(time = seq(262, 280, by = 2), inspections = 1)) |> summary()
 
 # Now plot effects of time
 
-pred %>% ggplot(aes(x = time, y = draw, colour = pandemic)) +
+pred |> ggplot(aes(x = time, y = draw, colour = pandemic)) +
   stat_lineribbon() +
   scale_fill_brewer(palette = "Blues") +
   scale_color_brewer(palette = "Dark2") +
@@ -201,23 +210,23 @@ pred %>% ggplot(aes(x = time, y = draw, colour = pandemic)) +
 pred1 <- predictions(m3, type = "response", re_formula = NULL,
                      newdata = datagrid(model = m3, pandemic = "Pre-Pandemic", 
                                         month = ts_model$month,
-                                        time = seq(1, 168, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(1, 168, by = 2), inspections = 1)) |> posteriordraws()
 head(pred1)
 
 pred2 <- predictions(m3, type = "response", re_formula = NULL,
                      newdata = datagrid(model = m3, pandemic = "Pandemic Period", 
                                         month = ts_model$month,
-                                        time = seq(169, 277, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(169, 280, by = 2), inspections = 1)) |> posteriordraws()
 
 pred <- rbind(pred1, pred2)
 remove(pred1, pred2)
 
-pred <- pred %>% mutate(month2 = month.abb[as.numeric(month)]) %>% 
-  mutate(month2 = as.factor(month2))  %>% 
+pred <- pred |> mutate(month2 = month.abb[as.numeric(month)]) |> 
+  mutate(month2 = as.factor(month2))  |> 
   mutate(month2 = fct_relevel(month2, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
                               "Aug", "Sep", "Oct", "Nov", "Dec"))
   
-pred %>% ggplot(aes(x = draw, y = month2, fill = pandemic)) +
+pred |> ggplot(aes(x = draw, y = month2, fill = pandemic)) +
   stat_halfeye(slab_alpha = .5) +
   scale_y_discrete(limits = rev) +
   labs(x = "Predicted Pass Rate", y = "", fill = "Time Period") +
@@ -227,7 +236,7 @@ pred %>% ggplot(aes(x = draw, y = month2, fill = pandemic)) +
 remove(pred)
 
 
-#### Model infraction rate
+#### Model infraction rate outcome
 
 get_prior(infractions | rate(inspections) ~ 0 + Intercept + time*pandemic + month,
           data = ts_model, family = negbinomial)
@@ -258,6 +267,7 @@ mcmc_plot(r1)
 mcmc_acf(r1, pars = vars(contains("Pandemic"), contains("time")), lags = 10)
 conditional_effects(r1)
 
+# Compare to model with auto-regressive term
 
 r2 <- brm(infractions  | rate(inspections) ~ 0 + Intercept + time*pandemic + month + ar(p = 1),
           data = ts_model, prior = prior, family = negbinomial,
@@ -274,7 +284,7 @@ conditional_effects(r2)
 
 loo(r1, r2)
 
-# Compare with multi-level approach for month
+# Compare with multi-level/varying effect approach for month
 
 r3 <- brm(infractions  | rate(inspections) ~ 0 + Intercept + time*pandemic + (1 | month),
           data = ts_model, prior = prior, family = negbinomial,
@@ -309,24 +319,24 @@ exp(fixef(r3)[,-2])
 
 get_variables(r3)
 
-r3 %>% gather_draws(r_month[month,]) %>%  mean_qi()
+r3 |> gather_draws(r_month[month,]) |>  mean_qi()
 
 
-# First effect of pandemic
+# First examine/plot effect of pandemic
 
 pred <- predictions(r3, type = "response", re_formula = NA,
                     newdata = datagrid(pandemic = c("Pre-Pandemic", "Pandemic Period"),
-                                       inspections = 1)) %>% posteriordraws()
+                                       inspections = 1)) |> posteriordraws()
 
 
 pred1 <- predictions(r3, type = "response",
                      newdata = datagrid(model = r3, pandemic = "Pre-Pandemic", 
-                                        time = seq(1, 168, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(1, 168, by = 2), inspections = 1)) |> posteriordraws()
 head(pred1)
 
 pred2 <- predictions(r3, type = "response",
                      newdata = datagrid(model = r3, pandemic = "Pandemic Period", 
-                                        time = seq(169, 277, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(169, 280, by = 2), inspections = 1)) |> posteriordraws()
 
 pred <- rbind(pred1, pred2)
 remove(pred1, pred2)
@@ -339,7 +349,7 @@ p1 <- ggplot(pred, aes(x = draw, fill = pandemic)) +
   theme(legend.position = "bottom")   
 
 mfx <- marginaleffects(r3, type = "response", variables = "pandemic", 
-                       newdata = datagrid(time = seq(1, 277, by = 2), inspections = 1)) %>% 
+                       newdata = datagrid(time = seq(1, 280, by = 2), inspections = 1)) |> 
   posteriordraws()
 
 p2 <- ggplot(mfx, aes(x = draw)) +
@@ -348,7 +358,7 @@ p2 <- ggplot(mfx, aes(x = draw)) +
        subtitle = "Marginal Effect of Pandemic") +
   theme_minimal() 
 
-cowplot::plot_grid(p1, p2, nrow=1, ncol=2)
+cowplot::plot_grid(p1, p2, labels = c("A", "B"), nrow=1, ncol=2)
 
 remove(p1, p2)
 
@@ -356,19 +366,19 @@ remove(p1, p2)
 # and over 2022 since inspection numbers resumed to 200+ in week 17 (Apr. 25, 2022)
 
 marginaleffects(r3, type = "response", variables = "pandemic",
-                        newdata = datagrid(time = seq(1, 277, by = 2), 
-                                           inspections = 1)) %>% summary()
+                        newdata = datagrid(time = seq(1, 280, by = 2), 
+                                           inspections = 1)) |> summary()
 
 marginaleffects(r3, type = "response", variables = "pandemic", 
-                newdata = datagrid(time = seq(1, 261, by = 2), inspections = 1)) %>% summary()
+                newdata = datagrid(time = seq(1, 261, by = 2), inspections = 1)) |> summary()
 
 marginaleffects(r3, type = "response", variables = "pandemic", 
-                newdata = datagrid(time = seq(262, 277, by = 2), inspections = 1)) %>% summary()
+                newdata = datagrid(time = seq(262, 280, by = 2), inspections = 1)) |> summary()
 
 
 # Now plot effects of time 
 
-pred %>% ggplot(aes(x = time, y = draw, colour = pandemic)) +
+pred |> ggplot(aes(x = time, y = draw, colour = pandemic)) +
   stat_lineribbon() +
   scale_fill_brewer(palette = "Blues") +
   scale_color_brewer(palette = "Dark2") +
@@ -384,23 +394,23 @@ pred %>% ggplot(aes(x = time, y = draw, colour = pandemic)) +
 pred1 <- predictions(r3, type = "response", re_formula = NULL,
                      newdata = datagrid(model = r3, pandemic = "Pre-Pandemic", 
                                         month = ts_model$month,
-                                        time = seq(1, 168, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(1, 168, by = 2), inspections = 1)) |> posteriordraws()
 head(pred1)
 
 pred2 <- predictions(r3, type = "response", re_formula = NULL,
                      newdata = datagrid(model = r3, pandemic = "Pandemic Period", 
                                         month = ts_model$month,
-                                        time = seq(169, 277, by = 2), inspections = 1)) %>% posteriordraws()
+                                        time = seq(169, 280, by = 2), inspections = 1)) |> posteriordraws()
 
 pred <- rbind(pred1, pred2)
 remove(pred1, pred2)
 
-pred <- pred %>% mutate(month2 = month.abb[as.numeric(month)]) %>% 
-  mutate(month2 = as.factor(month2))  %>% 
+pred <- pred |> mutate(month2 = month.abb[as.numeric(month)]) |> 
+  mutate(month2 = as.factor(month2))  |> 
   mutate(month2 = fct_relevel(month2, "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul",
                               "Aug", "Sep", "Oct", "Nov", "Dec"))
 
-pred %>% ggplot(aes(x = draw, y = month2, fill = pandemic)) +
+pred |> ggplot(aes(x = draw, y = month2, fill = pandemic)) +
   stat_halfeye(slab_alpha = .5) +
   scale_y_discrete(limits = rev) +
   labs(x = "Predicted Infraction Rate", y = "", fill = "Time Period") +
@@ -410,7 +420,7 @@ pred %>% ggplot(aes(x = draw, y = month2, fill = pandemic)) +
 remove(pred)
 
 
-## Sensitivity analysis
+## Sensitivity analysis of different prior values
 
 prior_s1 <- set_prior("normal(0,2)", class = "b")
 prior_s2 <- set_prior("normal(0,0.5)", class = "b")
